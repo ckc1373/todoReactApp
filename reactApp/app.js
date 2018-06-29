a7dbad76cdc5f8f796273233a5fcb87fe00380a5
ckc1373 <ccourtne@hamilton.edu>
2018-06-29 12:09:27 -0700

    dummy list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
+
+const dummyData = ["Lorem ipsum dolor sit amet,", " consectetur adipisicing elit, sed do eiusmod tempor ",
+                  "incididunt ut labore et dolore magna aliqua. Ut enim ad ", "inim veniam, quis nostrud exercitation ullam"]
+
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<li><button>X</button>{this.props.task}</li>)
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (<ul>
+      {dummyData.map((data) => <Todo task ={data}/>)}
+    </ul>);
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
