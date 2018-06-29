fdf3cb6a5648e757ee2cfc7502ac6e43979bd29d
ckc1373 <ccourtne@hamilton.edu>
2018-06-29 14:14:13 -0700

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
+const session = require('express-session')
+const mongoose = require('mongoose')
+
+mongoose.connection.on('error', function(){
+  console.log('error connecting to the database')
+})
+mongoose.connection.on('connected', function(){
+  console.log('successfully connected to the database')
+})
+mongoose.connect(process.env.MONGODB_URI)
 
 // This line makes the build folder publicly available.
 app.use(express.static('build'));
